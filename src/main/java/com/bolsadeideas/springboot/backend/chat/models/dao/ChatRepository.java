57e375db41fa84b43622c1286e1cee79ca96814b
lenz90 <lenz.90@gmail.com>
2019-11-24 17:56:56 -0500

    Se agrega los ultimos cambios

--- src/main/java/com/bolsadeideas/springboot/backend/chat/models/dao/ChatRepository.java
+++ src/main/java/com/bolsadeideas/springboot/backend/chat/models/dao/ChatRepository.java
@@ -8,5 +8,5 @@ import com.bolsadeideas.springboot.backend.chat.models.documents.Mensaje;
 
 public interface ChatRepository extends MongoRepository<Mensaje, String>{
 	
-    public List<Mensaje> findFirst10ByOrderByFechaDesc();
+    List<Mensaje> findFirst10ByOrderByFechaDesc();
 }
--- src/main/java/com/bolsadeideas/springboot/backend/chat/models/service/ChatService.java
+++ src/main/java/com/bolsadeideas/springboot/backend/chat/models/service/ChatService.java
@@ -6,6 +6,6 @@ import com.bolsadeideas.springboot.backend.chat.models.documents.Mensaje;
 
 public interface ChatService {
 
-	public List<Mensaje> obtenerUltimos10Mensajes();
-	public Mensaje guardar(Mensaje mensaje);
+	List<Mensaje> obtenerUltimos10Mensajes();
+	Mensaje guardar(Mensaje mensaje);
 }
